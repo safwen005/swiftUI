9f9d92238b7d56beb78701b6dd8ec0be95499976
sdfs <s>
2021-05-02 01:40:15 +0100

    Initial Commit

---
+++ swiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swiftUI
+//
+//  Created by Safwen Chaieb on 5/2/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
